5d3284e2b92baf412306045e7bb1ba578d4d8429
kent riggs <k2r3@proton.me>
2024-01-10 07:17:23 -0700

    added code to solve for tests

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 
 // Write your code here
+let num1 = 2, num2 = 31, multiply = num1 * num2;
+let random = Math.floor(Math.random() + 1)
+let num3 = 12, num4 = 3, mod = num3 / num4
+let max = Math.max(5, 10, 15, 20)
